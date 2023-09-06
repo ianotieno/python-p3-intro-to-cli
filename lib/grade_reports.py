8b9105640785acef5897e486811421e968f6bb76
ian otieno <ianotieno23@gmail.com>
2023-09-06 18:18:29 +0300

    update

--- lib/grade_reports.py
+++ lib/grade_reports.py
@@ -1,7 +1,13 @@
+import os
 
 def create_grade_report(student_grades):
+    # Ensure the 'reports' directory exists
+    if not os.path.exists('reports'):
+        os.makedirs('reports')
+    
+    student_name, grade = student_grades.split(', ')
     with open('reports/grade_report.txt', 'w') as gr:
-        gr.write(student_grades)
+        gr.write(f"Student name: {student_name}\nGrade: {grade}")
 
 if __name__ == '__main__':
     student_grades = input("Student name, grade: ")
